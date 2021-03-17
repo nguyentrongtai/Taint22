f62c11fd6616dd061f12311d7725809d9770d1c0
nguyentrongtai <36144675+nguyentrongtai@users.noreply.github.com>
2021-03-17 21:07:58 +0700

    Creat project

--- countZeroDigit.js
+++
@@ -1,7 +0,0 @@
-function countZeroDigit(a, b) {
-    var string = '';
-    for (let i = a; i <= b; i++) string += i;
-    let length = string.split("0").length;
-    if (length == 0) return 0;
-    else return length - 1
-}
